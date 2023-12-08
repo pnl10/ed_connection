adc656fa996a74083e6e80872c880a67d0f17a55
pnl10 <pnl10@georgetown.edu>
2023-12-08 11:45:33 -0500

    ML

--- Project.R
+++ Project.R
@@ -2,6 +2,14 @@ library(tidyverse)
 library(dplyr)
 library(readr)
 library(sf)
+library(recipes)
+library(tidymodels)
+library(broom)
+library(patchwork)
+library(ggplot2)
+library(tidyclust)
+library(Rfast)
+
 income_data<- read_csv("income.csv")
 
 census_data <- read.csv("census_data.csv")
@@ -57,8 +65,8 @@ ed_census_inc_percent_tract <- ed_census_inc %>%
   mutate(less_than_hs_percent = less_than_hs / Pop_18.24)
 
 hs_tract <- ed_census_inc_percent_tract %>%
-  select(less_than_hs_percent, GEOID)%>%
-  mutate(GEOID = sub("1400000US","",GEOID))
+  select(less_than_hs_percent, GEO_ID)%>%
+  mutate(GEOID = sub("1400000US","",GEO_ID))
 
 join_data2 <- left_join(x= geospatial_data, hs_tract, by = "GEOID")
 
@@ -72,3 +80,47 @@ library(patchwork)
 plot_together = broadband_plot + ed_plot
 
 plot_together
+
+#MACHINE LEARNING
+#prep the data
+
+ed_percent_rec <- recipe(percent_broadband ~ . data = ed_census_inc_percent_tract)
+
+set.seed(20201007)
+
+# create a split object
+data_split <- initial_split(data = ed_census_inc_percent_tract, prop = 0.75)
+
+# create the training and testing data
+data_train <- training(x = data_split)
+data_test <- testing(x = data_split)
+
+# create a recipe
+knn_rec <-
+  recipe(formula =, data = data1_train)
+
+# create a knn model specification
+knn_mod <-
+  nearest_neighbor(neighbors = 5) %>%
+  set_engine(engine = "kknn") %>%
+  set_mode(mode = "regression")
+
+# create a workflow
+knn_wf <-
+  workflow() %>%
+  add_recipe(knn_rec) %>%
+  add_model(knn_mod)
+
+# fit the knn model specification on the training data
+knn_fit <- knn_wf %>%
+  fit(data = data1_train)
+
+# use the estimated model to predict values in the testing data
+predictions <-
+  bind_cols(
+    data_test,
+    predict(object = knn_fit, new_data = data_test)
+    )
+
+# calculate the rmse on the testing data
+rmse(data = predictions, truth = percent_broadband, estimate = .pred)
